e821dbc14ac128b0e5b612358bc206ba110b45d3
andre.teixeira <andre.teixeira@fncit.com.br>
2022-10-25 15:55:02 -0300

    [ALT] Projeto base

---
+++ src/main/java/br/com/unike/UnikeApplication.java
@@ -0,0 +1,13 @@
+package br.com.unike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/unike/UnikeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
